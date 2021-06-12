ef6e98a2bc1e9df1b8d3f6c8a126c39606d1dabe
davnin <nindorera26@outlook.com>
2021-06-12 13:44:58 -0400

    Lab3

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            ViewData["bottles"] = Request.Form["bottles"];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson()
+        {
+            ViewBag.fName = Request.Form["fName"];
+            ViewBag.lName = Request.Form["lName"];
+            ViewBag.age = Request.Form["age"];
+            ViewBag.email = Request.Form["email"];
+            ViewBag.dob = Request.Form["dob"];
+            ViewBag.password = Request.Form["password"];
+            ViewBag.description = Request.Form["description"];
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
