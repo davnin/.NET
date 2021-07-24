7292e0b08107fc39ce2ac542bd4cd102020a8280
DavidN <nindorera26@outlook.com>
2021-07-23 23:59:04 -0400

    Lab6 Final

---
+++ Lab6/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab6.Models
+{
+    public class Student : StudentBase
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Student Number")]
+        public Guid Id { get; set; }
+    }
+}
